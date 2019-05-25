897ab79fcf22262458dd62eb849d552d80a4749c
Mauro Stehle <mauro.stehle@students.fhnw.ch>
2019-05-25 22:32:00 +0200

    - added documentation

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # pdforganizer
 Automate PDF organization based on defined keywords
 
+## Requirements
+
+- Python 3
+- Searchable PDFs
+
+## How to use
+
+- save config_example.yaml as config.yaml.
+- define the folders for the source pdfs and where to save the guessed pdf.
+- you can define keyword which appear in the documents. These will be used for document names.
+- install the required libraries defined in requirements.txt: (navigate to the folder, pip install -r requirements.txt)
+- run pdf_organizer.py
+
+
+
+
+
--- pdf_organizer.py
+++ pdf_organizer.py
@@ -2,7 +2,6 @@
 import PyPDF2
 import os
 
-import nltk
 from nltk import word_tokenize
 from nltk import FreqDist
 import yaml
@@ -21,28 +20,25 @@ target_folder = Path(config['target_folder'])
 default_folder = Path(config['default_folder'])
 original_move_folder = Path(config['original_move_folder'])
 folder_keywords = config['folders']
-split_pdf = config['split_pdf']
+split_pdf_conf = config['split_pdf']
 print_paths = config['print_paths']
 
-
-#analyzer = TfidfVectorizer().build_analyzer()
-
 umlauts = {
-    ord('Ä'):'Ae',
-    ord('ä'):'ae',
-    ord('Ö'):'Oe',
-    ord('ö'):'oe',
-    ord('Ü'):'Ue',
-    ord('ü'):'ue'
-    }
+    ord('Ä'): 'Ae',
+    ord('ä'): 'ae',
+    ord('Ö'): 'Oe',
+    ord('ö'): 'oe',
+    ord('Ü'): 'Ue',
+    ord('ü'): 'ue'
+}
 
 
 def import_stopwords(path):
-    stopwords_d = []
+    stopwords = []
     with open(path, 'r', encoding='utf_8') as file:
         for line in file:
-            stopwords_d.append(line.replace('\n','').lower().translate(umlauts))
-    return stopwords_d
+            stopwords.append(line.replace('\n', '').lower().translate(umlauts))
+    return stopwords
 
 
 stopwords_d = import_stopwords("GermanST_utf8.txt")
@@ -55,19 +51,19 @@ def translate_umlauts(doc):
 def split_pdfs(directory):
     for file in os.listdir(directory):
         if str(file).endswith('.pdf'):
-            split_pdf(str(os.path.join(directory,file)))
+            split_pdf(str(os.path.join(directory, file)))
 
 
 def split_pdf(path):
-    pdf_file = open(path,'rb')
+    pdf_file = open(path, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-    pageNumbers = pdf_reader.getNumPages()
+    page_numbers = pdf_reader.getNumPages()
     now = datetime.datetime.now()
 
-    for i in range (pageNumbers):
+    for i in range(page_numbers):
         pdf_writer = PyPDF2.PdfFileWriter()
         pdf_writer.addPage(pdf_reader.getPage(i))
-        file_name = now.strftime("%Y-%m-%d") + "_" + str(now.microsecond) + "_" + 'Scan_' + str(i+1) + '.pdf'
+        file_name = "{}_{}_{}.pdf".format(now.strftime("%Y-%m-%d"), now.microsecond, i + 1)
         file = open(str(os.path.join(source_folder, file_name)), 'wb')
         pdf_writer.write(file)
         file.close()
@@ -75,7 +71,8 @@ def split_pdf(path):
 
     if not os.path.exists(str(os.path.join(original_move_folder))):
         os.makedirs(str(os.path.join(original_move_folder)))
-    os.rename(path, str(os.path.join(original_move_folder, now.strftime("%Y-%m-%d") + "_" + str(now.microsecond) +'_Scan.pdf')))
+    os.rename(path, str(
+        os.path.join(original_move_folder, now.strftime("%Y-%m-%d") + "_" + str(now.microsecond) + '_Scan.pdf')))
 
 
 def convert_pdf_to_txt(path):
@@ -85,7 +82,6 @@ def convert_pdf_to_txt(path):
 
 
 def tokenize_pdf(path):
-    text = []
     text = convert_pdf_to_txt(path)
     tokens = [word for word in word_tokenize(text) if word not in stopwords_d and len(word) > 2]
     return tokens
@@ -97,8 +93,8 @@ def tokenize_pdfs(directory):
     for subdir, dirs, files in os.walk(directory):
         for file in files:
             if str(file).endswith('.pdf'):
-                if split_pdf:
-                    split_pdf(str(os.path.join(subdir, file)))
+                if split_pdf_conf:
+                    split_pdf_conf(str(os.path.join(subdir, file)))
                 tags.append(subdir.replace(directory, '').split(os.sep))
                 tokens.append(tokenize_pdf(str(os.path.join(subdir, file))))
 
@@ -106,15 +102,16 @@ def tokenize_pdfs(directory):
 
 
 def categorize_pdfs(directory):
+    global folder
     for file in os.listdir(directory):
         if str(file).endswith('.pdf'):
             tokens = tokenize_pdf(os.path.join(directory, str(file)))
-            fdist = FreqDist(word.lower() for word in tokens)
+            f_dist = FreqDist(word.lower() for word in tokens)
             ranking = {}
             keywords = {}
             for folder in folder_keywords:
-                ranking[folder] = sum([fdist[keyword] for keyword in folder_keywords[folder]])
-                keywords[folder] = [(keyword, fdist[keyword]) for keyword in folder_keywords[folder]]
+                ranking[folder] = sum([f_dist[keyword] for keyword in folder_keywords[folder]])
+                keywords[folder] = [(keyword, f_dist[keyword]) for keyword in folder_keywords[folder]]
             sorted_ranking = sorted(ranking.items(), key=lambda kv: kv[1], reverse=True)
             if sorted_ranking[0][1] == 0:
                 if not os.path.exists(default_folder):
@@ -123,27 +120,28 @@ def categorize_pdfs(directory):
                 if print_paths:
                     print(str(os.path.join(default_folder, file)))
             else:
-                keyword = sorted(keywords[sorted_ranking[0][0]],key=lambda kv: kv[1], reverse=True)[0][0]
+                keyword = sorted(keywords[sorted_ranking[0][0]], key=lambda kv: kv[1], reverse=True)[0][0]
                 if not os.path.exists(str(os.path.join(target_folder, sorted_ranking[0][0]))):
                     os.makedirs(str(os.path.join(target_folder, sorted_ranking[0][0])))
                 filename = create_filename(folder, keyword)
-                os.rename(str(os.path.join(directory, file)), str(os.path.join(target_folder, sorted_ranking[0][0],filename)))
+                os.rename(str(os.path.join(directory, file)),
+                          str(os.path.join(target_folder, sorted_ranking[0][0], filename)))
                 if print_paths:
-                    print(str(os.path.join(target_folder, sorted_ranking[0][0],filename)))
+                    print(str(os.path.join(target_folder, sorted_ranking[0][0], filename)))
 
 
 def create_filename(folder, key):
-        now = datetime.datetime.now()
-        filename = config['save_pattern']
-        filename = filename.replace('%d', now.strftime(config['date_format']))
-        filename = filename.replace('%k', key)
-        filename = filename.replace('%f', folder)
-        filename = filename.replace('%m', str(now.microsecond))
-        filename = filename + ".pdf"
-        return filename
+    now = datetime.datetime.now()
+    filename = config['save_pattern']
+    filename = filename.replace('%d', now.strftime(config['date_format']))
+    filename = filename.replace('%k', key)
+    filename = filename.replace('%f', folder)
+    filename = filename.replace('%m', str(now.microsecond))
+    filename = filename + ".pdf"
+    return filename
 
 
-if split_pdf:
+if split_pdf_conf:
     split_pdfs(source_folder)
 
 categorize_pdfs(source_folder)
